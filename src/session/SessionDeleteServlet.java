47e2287eab95dcaa09008a2852640a87298753cd
mindo413 <mindo413@naver.com>
2019-11-11 17:15:00 +0900

    .

--- src/session/SessionDeleteServlet.java
+++ src/session/SessionDeleteServlet.java
@@ -9,20 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 @WebServlet("/session/delete")
 public class SessionDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		// 세션 객체 얻기
 		HttpSession session = req.getSession();
-		
+
 		// 세션 삭제
 		session.invalidate();
-		
-		req.getRequestDispatcher("/WEB-INF/views/session/delete.jsp").forward(req, resp);;
+
+		req.getRequestDispatcher("/WEB-INF/views/session/delete.jsp").forward(req, resp);
+		;
 	}
 }
